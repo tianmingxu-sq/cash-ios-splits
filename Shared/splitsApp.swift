215dafb5b977c7784fca5dd223e403ef7ab2880b
Tianming Xu <tianmingxu@squareup.com>
2022-06-22 14:15:44 -0700

    Initial Commit

---
+++ Shared/splitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  splitsApp.swift
+//  Shared
+//
+//  Created by Tianming Xu on 6/22/22.
+//
+
+import SwiftUI
+
+@main
+struct splitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
